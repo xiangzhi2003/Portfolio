f512623adf1b69a131c8f2c22da77a73d82c248c
CHIANG XIANG ZHI <xiangzhichiang2003@gmail.com>
2026-01-27 23:29:02 +0800

    Add about content

---
+++ src/about/content.ts
@@ -0,0 +1,6 @@
+export const aboutParagraphs = [
+  "I'm a passionate Software Engineering student at Asia Pacific University specializing in building robust, scalable backend systems and also frontend applications.",
+  "My journey in tech began with a deep curiosity about how systems function and has evolved into a commitment to high-quality development.",
+  "I am dedicated to creating impactful software solutions that address real-world challenges through clean code and innovative design.",
+];
+
